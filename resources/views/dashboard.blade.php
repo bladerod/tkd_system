5bc2f767e6d0ee62e46b15f1e5c0140cb2ca09e4
bladerod <custodio.nathaniel.custodio@gmail.com>
2026-03-31 08:08:21 +0800

    commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -458,7 +458,7 @@
                             <div>
                                 <label class="block text-sm font-medium text-gray-700 mb-1">Branch <span class="text-red-500">*</span></label>
                                 <select name="branch_id" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-[#1c1c1d] focus:border-[#1c1c1d] text-gray-700">
-                                        <option value="" disabled selected>Select gender</option>
+                                        <option value="" disabled selected>Select Branch</option>
                                     @foreach ($branches as $branch)
                                         <option value="{{ $branch->id }}">{{ $branch->code.' '.$branch->name.' '.$branch->city }}</option>
                                     @endforeach

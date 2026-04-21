87af5d3a3241584f4171c6981f2a543603f178e8
bladerod <custodio.nathaniel.custodio@gmail.com>
2026-04-21 17:38:10 +0800

    billing fixed

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -15,6 +15,7 @@ class Payment extends Model
         'payment_method', 
         'transaction_reference', 
         'paid_at', 
+        'paid_by_user_id',
         'status'
     ];
     protected $casts = [
